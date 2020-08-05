8cb6ab420a58f60c33d8d3f8226e3034f4eb38a8
Khurram <khurram@bromine.universe.hm>
2020-08-05 14:41:59 -0400

    Finished 48

--- python-masterclass-remaster/ProjectFlow/guessinggame.py
+++ python-masterclass-remaster/ProjectFlow/guessinggame.py
@@ -1,11 +1,34 @@
 answer = 5
 
 #print("Please guess number between 1 and 10: ")
+#guess = int(input())
 guess = int(input("Please guess number between 1 and 10: "))
-
-if guess < answer:
-    print("Please guess higher")
-elif guess > answer:
-    print("Please guess lower")
+if guess != answer:
+    if guess < answer:
+        print("Please guess higher")
+    else:    # guess must be grater than answer
+        print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it")
+    else:
+        print("Sorry, you have not guessed correctly")
 else:
-    print("You got it first time!")
+    print("You got it first time")
+
+# if guess < answer:
+#     print("Please guess higher")
+#     guess = int(input("Please guess number between 1 and 10: "))
+#     if  guess == answer:
+#         print("Well Done! You guessed right!")
+#     else:
+#         print("Sorry, you have not guessed correctly")
+# elif guess > answer:
+#     print("Please guess lower")
+#     guess = int(input("Please guess number between 1 and 10: "))
+#     if  guess == answer:
+#         print("Well Done! You guessed right!")
+#     else:
+#         print("Sorry, you have not guessed correctly")
+# else:
+#     print("You got it first time!")
